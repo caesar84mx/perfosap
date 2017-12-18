eb235c1ae8c96d29d0290c51f3d0d317720cf5a2
caesar84mx <dymnov.dm@gmail.com>
2017-12-18 19:55:43 -0400

    Adding HandoutOrder entity: a model ready

--- almacen/models.py
+++ almacen/models.py
@@ -1,8 +1,7 @@
-import json
+import datetime
 
 from django.db import models
 from django.urls import reverse
-
 
 # Create your models here.
 from procura.models import Item
@@ -20,6 +19,7 @@ class StoredItem(models.Model):
 
     def get_absolute_url(self):
         return reverse('almacen:storeditem-detail', args=[str(self.id)])
+
 
 OPERATIONS = 'OPS'
 PROCUREMENT = 'PRO'
@@ -62,17 +62,33 @@ ORDER_STATUS = {
 
 class HandoutOrder(models.Model):
     department = models.CharField(max_length=3, choices=DEPARTMENT_CHOICE, default=OPERATIONS)
-    datetime_registered = models.DateTimeField()
+    datetime_registered = models.DateTimeField(default=datetime.datetime.now())
     status = models.CharField(max_length=3, choices=ORDER_STATUS, default=PENDING)
-    change_status_datetime = models.DateTimeField()
-    #items list will be json and must have the following format "[{stored_item.pk: quantity}, ...]"
-    items = models.TextField()
-
-    def setitems(self, items_string):
-        self.items = json.dumps(items_string)
-
-    def getitems(self):
-        return json.loads(self.items)
+    change_status_datetime = models.DateTimeField(default=datetime.datetime.now())
+    items = models.ManyToManyField(Item, through='ItemToHandout')
 
     class Meta:
-        ordering = ['-datetime_registered']
+        ordering = ['-datetime_registered', '-change_status_datetime']
+
+    def __str__(self):
+        return "Registered: {0}\n" \
+               "Department: {1}\n" \
+               "Status: {2}\n" \
+               "Status changed: {3}\n" \
+               "Items: {4}".format(self.datetime_registered,
+                                            self.department,
+                                            self.status,
+                                            self.change_status_datetime,
+                                            self.items)
+
+
+class ItemToHandout(models.Model):
+    handoutorder = models.ForeignKey(HandoutOrder)
+    item = models.ForeignKey(Item)
+    quantity = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['item']
+
+    def __str__(self):
+        return "Order {0}: {1} - {2}, {3} pcs.".format(self.handoutorder.pk, self.item.serial, self.item.name, self.quantity)
